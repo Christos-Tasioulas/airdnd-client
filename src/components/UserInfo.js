d1d10bea3db9d150883467a57a7db214ae3d5e30
User <christasioulas2@gmail.com>
2023-07-26 20:14:52 +0300

    Made Approve form functional

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -10,7 +10,14 @@ export default function UserInfo() {
         fetch(`http://localhost:5000/user/getUserById/${id}`)
             .then((response) => response.json())
             .then((data) => setUser(data.message))
-    }, [])
+    }, [id])
+
+    const [formData, setFormData] = useState({
+        isApproved: user.isApproved,
+        isEmpty: true,
+        isSubmitted: false
+    })
+    const [message, setMessage] = useState("")
 
     const contacts = [
         {id:1, favicon: 'https://cdn3.iconfinder.com/data/icons/social-messaging-ui-color-line/245532/72-512.png', alt:"email", name:user.email},
@@ -23,6 +30,41 @@ export default function UserInfo() {
             <h3>{contact.name}</h3>
         </div>
     )
+
+    function handleClick(event) {
+        const {name, value} = event.target
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            isEmpty: false,
+            isSubmitted: false,
+            [name]: value
+        }))
+
+        const approveMessage = (value === 'true') ? "Landlord Approved!" : "Landlord Disapproved!"
+        setMessage(approveMessage)
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        setFormData(prevState => ({
+            ...prevState,
+            isSubmitted: true
+        }))
+
+        setUser(prevState => ({
+            ...prevState,
+            isApproved: formData.isApproved
+        }))
+        
+        const requestOptions = {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({id:user.id, isApproved:formData.isApproved})
+        }
+
+        fetch('http://localhost:5000/user/approveUser', requestOptions)
+    }
 
     return (
         <main className="App-profile-container">
@@ -48,21 +90,28 @@ export default function UserInfo() {
                         {user.isTenant && <div className='App-profile-tenant'>
                             <h3>Tenant Info</h3>
                         </div>}
-                        {!user.isApproved && <form className='App-approve-user'>
+                        {user.isLandlord && <form className='App-approve-user' onSubmit={handleSubmit}>
                             <h3>Approved to be landlord?</h3>
                             <div className='App-approval-radios'>
                                 <div className='App-approval-radio'>
-                                    <input type="radio" id="Yes" name="isApproved" value={user.isApproved} />
+                                    <input type="radio" id="Yes" name="isApproved" value="true" onClick={handleClick}/>
                                     <label htmlFor="Yes">Yes</label>
                                 </div>
                                 <div className='App-approval-radio'>
-                                    <input type="radio" id="No" name="isApproved" value={user.isApproved} />
+                                    <input type="radio" id="No" name="isApproved" value="false" onClick={handleClick}/>
                                     <label htmlFor="No">No</label>
                                 </div>      
                             </div>
                             <br /><br />
-                            <button className='App-approval-submit'>Save Changes</button>
+                            <button 
+                                className='App-approval-submit' 
+                                style={{
+                                    backgroundColor: formData.isEmpty ? "#484848" : "#ff585d",
+                                    cursor: formData.isEmpty && "default"
+                                }}
+                            >Save Changes</button>
                         </form>}
+                        {message !== "" && formData.isSubmitted && <h3>{message}</h3>}
                     </div>
                 </div>
             </div>
