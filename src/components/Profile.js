8f5e9eaf2478c2c1f3fc0380da919add442bfe53
User <christasioulas2@gmail.com>
2023-08-15 19:21:32 +0300

    Now users can see their profile info

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './Profile.css'
 import { Link } from 'react-router-dom';
 
-export default function Profile() {
+export default function Profile(props) {
+    
+    const currentUser = props.user 
 
     // All the favicons shown in the contact section of the user profile
     const contacts = [
-        {id:1, favicon: 'https://cdn3.iconfinder.com/data/icons/social-messaging-ui-color-line/245532/72-512.png', alt:"email", name:'email@email.com'},
-        {id:2, favicon: 'https://cdn.icon-icons.com/icons2/644/PNG/512/red_phone_icon-icons.com_59526.png', alt:"phone", name:'565656565656'}
+        {id:1, favicon: 'https://cdn3.iconfinder.com/data/icons/social-messaging-ui-color-line/245532/72-512.png', alt:"email", name: currentUser.email},
+        {id:2, favicon: 'https://cdn.icon-icons.com/icons2/644/PNG/512/red_phone_icon-icons.com_59526.png', alt:"phone", name: currentUser.phoneNumber}
     ]
 
     // User contact information as html elements
@@ -40,12 +42,12 @@ export default function Profile() {
                     <div className="App-profile-userInfo">
                         {/* Displays the profile image of the user. For now it is the default profile image, profile images are not working yet in the app. */}
                         <div className="App-profile-image">
-                            <img src="https://i.pinimg.com/originals/46/72/f8/4672f876389036583190d93a71aa6cb2.jpg" alt="prof"/>
+                            <img src={currentUser.image === "" ? "https://i.pinimg.com/originals/46/72/f8/4672f876389036583190d93a71aa6cb2.jpg" : currentUser.image} alt="prof"/>
                         </div>
                         {/* Important User Info */}
-                        <h2 className='App-profile-fullname'>First Name Last Name</h2>
-                        <h2 className='App-profile-username'>Username</h2>
-                        <h3 className='App-profile-role'>Role(s)</h3>
+                        <h2 className='App-profile-fullname'>{currentUser.firstname} {currentUser.lastname}</h2>
+                        <h2 className='App-profile-username'>{currentUser.username}</h2>
+                        <h3 className='App-profile-role'>{currentUser.isAdmin && "Admin"} {currentUser.isTenant && "Tenant"} {currentUser.isLandlord && "Landlord"}</h3>
                         <br /><br /><br />
                         {/* Contact Section in the profile page */}
                         <div className='App-profile-contacts'>
@@ -56,12 +58,13 @@ export default function Profile() {
                     {/* These are set to appear depending on the roles of the user, the user authentication is not ready yet */}
                     {/* We don't have the corresponding info yet in the database */}
                     <div className='App-profile-userHistory'>
-                        <div className='App-profile-landlord'>
+                        {currentUser.isLandlord && <div className='App-profile-landlord'>
                             <h3>Landlord Info</h3>
-                        </div>
-                        <div className='App-profile-tenant'>
+                            {!currentUser.isApproved && <h2>Landlord not approved yet!!</h2>}
+                        </div>}
+                        {currentUser.isTenant && <div className='App-profile-tenant'>
                             <h3>Tenant Info</h3>
-                        </div>
+                        </div>}
                     </div>
                 </div>
             </div>
