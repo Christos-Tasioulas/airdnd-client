335f29c2b69252e771b519b8dfbedfe4f9be1495
User <christasioulas2@gmail.com>
2023-08-27 02:12:36 +0300

    running script updated accordingly

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 set "HTTPS=true"
-set "SSL_CRT_FILE=../airdnd-server/certificates/cert.pem"
-set "SSL_KEY_FILE=../airdnd-server/certificates/key.pem"
+set "SSL_CRT_FILE=../airdnd-server/configurations/cert.pem"
+set "SSL_KEY_FILE=../airdnd-server/configurations/key.pem"
 npm start
